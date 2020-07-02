81a5006f972a15a391d76b3dd8e50801b782ecfe
Fangfan Li <fangfanli28@gmail.com>
2020-07-02 14:19:13 -0400

    fixed directory

--- src/restartServers.sh
+++ src/restartServers.sh
@@ -5,7 +5,7 @@ screen -X -S replay quit
 
 sleep 5
 
-cd /home/ubuntu/wehe_desktop_py3/src/
+cd /home/ubuntu/wehe-py3/src/
 
 screen -S analyzer -d -m sudo python3 replay_analyzerServer.py --ConfigFile=configs.cfg --original_ports=True
 
