df0c60d348febb48b52c4e5f261e65571181bc5f
zeinabshmeis <zeinab.shmeis@epfl.ch>
2023-12-14 19:17:38 +0100

    add the localization test

--- src/replay_server.py
+++ src/replay_server.py
@@ -1870,6 +1870,7 @@ def run(*args):
     bqResultWriter.create_replayInfo_schema()
     bqResultWriter.create_clientXputs_schema()
     bqResultWriter.create_decisions_schema()
+    bqResultWriter.create_localizeDecisions_schema()
 
     if configs.get('iperf'):
         LOG_ACTION(logger, 'Starting iperf server')
